898df7db0d531fa7c3212757067d4a26838c74e2
jinqiangshou <271021733@qq.com>
2014-11-25 11:03:17 +0800

    version and author information

---
+++ src/version.h
@@ -0,0 +1,10 @@
+#ifndef __VERSION_H__
+#define __VERSION_H__
+
+#define VERSION "Tri-prong 0.1"
+#define LAST_UPDATE "Nov. 21st, 2014"
+#define AUTHOR "Horst Xu"
+#define CONTACT "271021733@qq.com"
+
+
+#endif //__VERSION_H__
