903a91e9836960ba619869b219d71d67b0ab2af1
jinqiangshou <271021733@qq.com>
2014-12-02 15:24:45 +0800

    Update comm_const.h

--- src/comm_const.h
+++ src/comm_const.h
@@ -1,12 +1,13 @@
 #ifndef __COMM_CONST_H__
 #define __COMM_CONST_H__
 
-#define MAX_WAIT_EVENT 500
-#define MAXDATASIZE 100
+#define MAX_WAIT_EVENT 20000
+#define MAX_EPOLL_WAIT_TIME 2 //unit: ms
+#define REQUEST_TIME_OUT 3 //unit: second. Request failed if it's not replied within 3 seconds.
 #define MAX_URL_SIZE 1000
 #define MAX_REQUEST_SIZE 2048
-#define MAX_RECV_SIZE_ONE_TIME 8192
-#define MAXRATE 5000
-#define MAXTIME 3600
+#define MAX_RECV_SIZE_ONE_TIME 8192 //unit: bytes
+#define MAX_RATE 5000
+#define MAX_TIME 100
 
 #endif //__COMM_CONST_H__
