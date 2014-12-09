02e84ccb6d06b747223e6e136c55cc898b5332aa
jinqiangshou <271021733@qq.com>
2014-12-09 17:53:45 +0800

    Update comm_struct.h

--- src/comm_struct.h
+++ src/comm_struct.h
@@ -1,5 +1,5 @@
-#ifndef __COMM_STRUCT_H__
-#define __COMM_STRUCT_H__
+#ifndef __COMMON_STRUCT_H__
+#define __COMMON_STRUCT_H__
 
 #include <sys/time.h>
 
@@ -8,34 +8,27 @@ typedef struct __parameters_set {
 	int __press_rate;
 	int __press_time;
 	char *__host_ip;
-	short __port;
+	unsigned short __port;
 	char *__uri;
 } parameters_set;
 
 //存储主机信息
 typedef struct __host_info {
 	char *host_ip;
-	short port;
+	unsigned short port;
 	char *uri;
 } host_info;
 
-//存储socket描述符以及该请求在request_data数组中对应的位置
-//该结构体被用于在管道中传输
-typedef struct __req_pos_pair {
-	int sockfd;
-	int position;
-} req_pos_pair;
-
 //用于存储每次连接的数据
 typedef struct __request_data {
-	short is_connected; //TCP请求是否连接成功,压测执行时只能被thread_send更改，只能被thread_send读
-	short is_replied; //请求是否收到回复,压测执行时只能被thread_recv更改，只能被thread_recv读
-	timeval init_connect; //只能被thread_conn更改，会被thread_send和thread_recv读
+	short is_connected; //TCP请求是否连接成功,压测执行时只能被thread_send更改，只能被thread_send读。-1表示出错，0表示未连接或超时，1表示成功
+	short is_replied; //请求是否收到回复,压测执行时只能被thread_recv更改，只能被thread_recv读。-1表示出错，0表示未连接或超时，1表示成功
+	timeval init_connect; //能被thread_conn更改，会被thread_send和thread_recv读，用于超时检测
 	unsigned int connect_time; //只能被thread_send更改
 	unsigned int start_send_time; //只能被thread_send更改
 	unsigned int recv_finish_time; //只能被thread_recv更改
 	unsigned long byte_transfered; //只能被thread_recv更改，单位为字节
-	int sockfd; //本data属于哪个socket，只能被thread_conn更改，被thread_send和thread_recv读
+	int sockfd; //本data属于哪个socket，只能被thread_conn更改，被thread_send和thread_recv读，如果sockfd=-1，证明该socket使用过并且已经被关闭
 } request_data;
 
 //用于最终输出的数据统计，以下结构体所有元素只能被main函数更改
@@ -62,4 +55,5 @@ typedef struct __final_statistics {
 	double fail_percent; // equal to ( total_fail_count / total_request ) * 100%
 	unsigned long total_byte;
 } final_statistics;
-#endif //__COMM_STRUCT_H__
+
+#endif //__COMMON_STRUCT_H__
