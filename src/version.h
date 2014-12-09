51570b598c0b19796e3c59511d615ef06aa0cdf6
jinqiangshou <271021733@qq.com>
2014-12-09 17:55:53 +0800

    Update version.h

--- src/version.h
+++ src/version.h
@@ -1,8 +1,8 @@
 #ifndef __VERSION_H__
 #define __VERSION_H__
 
-#define VERSION "Tri-prong 0.1"
-#define LAST_UPDATE "Nov. 21st, 2014"
+#define VERSION "Tri-prong 1.0"
+#define LAST_UPDATE "Dec. 9th, 2014"
 #define AUTHOR "Horst Xu"
 #define CONTACT "271021733@qq.com"
 
