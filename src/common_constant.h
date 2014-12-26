9dfec423892bdf3c59b13da5483d5d951df0caad
jinqiangshou <271021733@qq.com>
2014-12-26 15:20:49 +0800

    Update common_constant.h

--- src/common_constant.h
+++ src/common_constant.h
@@ -1,13 +1,17 @@
-#ifndef __COMM_CONST_H__
-#define __COMM_CONST_H__
+#ifndef __COMMON_CONSTANT_H__
+#define __COMMON_CONSTANT_H__
 
 #define MAX_WAIT_EVENT 20000
-#define MAX_EPOLL_WAIT_TIME 2 //unit: ms
+#define MAX_EPOLL_WAIT_TIME 1 //unit: ms
 #define REQUEST_TIME_OUT 3 //unit: second. Request failed if it's not replied within 3 seconds.
+#define REQUEST_EXPIRE_TIME 500000 //unit: us ( NOTE: 1s = 1000000us ). If recv generates EAGAIN, we still listen the socket in epoll until it expires. 
 #define MAX_URL_SIZE 1000
 #define MAX_REQUEST_SIZE 2048
 #define MAX_RECV_SIZE_ONE_TIME 8192 //unit: bytes
 #define MAX_RATE 10000
 #define MAX_TIME 100
+#define MAX_URL_NUMBER 2
+#define MAX_HOST_IP_LENGTH 50
+#define MAX_URI_LENGTH 500
 
-#endif //__COMM_CONST_H__
+#endif //__COMMON_CONSTANT_H__
