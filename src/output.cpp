bd09fb238212569126b8c6dfdd4e1938d1a94d51
jinqiangshou <271021733@qq.com>
2014-12-26 15:21:36 +0800

    Update output.cpp

--- src/output.cpp
+++ src/output.cpp
@@ -8,32 +8,32 @@
 
 extern int press_rate;
 extern double press_interval;
-extern host_info host;
 extern int press_time;
 
 //如何使用本工具
 void usage( const char* tool_name )
 {
-	fprintf( stderr, "usage:  %s\n", tool_name );
-	fprintf( stderr, "        -r [or --rate] N                 [REQUIRED] specify the press rate (unit: query per second).\n" ); 
-	fprintf( stderr, "        -t [or --time] N                 [REQUIRED] test time (unit: second). \n" );
-	fprintf( stderr, "        -h [or --host] XXX.XXX.XXX.XXX   target host ip. The default host ip is 127.0.0.1 .\n" );
-	fprintf( stderr, "        -p [or --port] N                 port number. The default port number is 80.\n" );
-	fprintf( stderr, "        -u [or --uri ] /index.html       uri of the request.\n" );
+	fprintf( stderr, "Usage: %s\n", tool_name );
+	fprintf( stderr, "       -r [--rate] M                                  press rate (unit: query per second).\n" ); 
+	fprintf( stderr, "       -t [--time] N                                  test time (unit: second). \n" );
+	fprintf( stderr, "       -u [--url ] http://XX.XXX.XXX.XX:80/index.html request url, default port number 80.\n" );
 	exit( 1 );
 }
 
 //打印读取的参数信息
-void print_info( void )
+void print_info( parameters_set *parameters )
 {
 	fprintf( stdout, "\n" );
 	fprintf( stdout, "This is %s (last updated on %s), an HTTP benchmarking tool.\n", VERSION, LAST_UPDATE );
 	fprintf( stdout, "Copyright © 2014 %s. Email: %s\n\n", AUTHOR, CONTACT );
 	fprintf( stdout, "Test Information\n" );
-	fprintf( stdout, "    Host       : %s\n", host.host_ip );
-	fprintf( stdout, "    Port       : %d\n", host.port );
-	fprintf( stdout, "    URI        : %s\n", host.uri );
-	fprintf( stdout, "    Target QPS : %-6d%-11s\n", press_rate, "[#/second]" );
+	if( parameters->url_number == 1 )
+	{
+		fprintf( stdout, "    Host       : %s\n", parameters->host[0]->host_ip );
+		fprintf( stdout, "    Port       : %d\n", parameters->host[0]->port );
+		fprintf( stdout, "    URI        : %s\n", parameters->host[0]->uri );
+	}
+	fprintf( stdout, "    Target RPS : %-6d%-11s\n", press_rate, "[#/second]" );
 	fprintf( stdout, "    Test Time  : %-6d%-11s\n", press_time, "[second]" );
 	fprintf( stdout, "\n" );
 	return;
@@ -54,6 +54,37 @@ void check_setting( void )
 	}
 	
 	return ;
+}
+
+void check_connect_ontime( const int press_number, const test_connect_ontime *tco )
+{
+	if( press_number < 100 )
+	{
+		return ;
+	}
+	int temptime_us = (tco->test_end).tv_usec - (tco->test_start).tv_usec;
+	int temptime_s = (tco->test_end).tv_sec - (tco->test_start).tv_sec;
+	if ( temptime_us < 0 )
+	{
+		temptime_s--;
+		temptime_us += 1000000;
+	}
+	int actual_rps = (int)( press_number / (temptime_s + temptime_us / 1000000.0) );
+	double err = actual_rps*1.0 / press_rate -1;
+	if( err > 0.05 || err < -0.05)
+	{
+		fprintf( stderr, "ATTENTION: The actual RPS did NOT reach your Target RPS %d [#/second].\n", press_rate );
+		fprintf( stderr, "           Actually, %d requests were sent out within %d.%d seconds.\n", press_number, temptime_s, (temptime_us/1000) );
+		fprintf( stderr, "           Therefore, the actual RPS is about %d [#/second].\n", actual_rps);
+		fprintf( stderr, "\n");
+	}else
+	{
+		fprintf( stdout, "The actual RPS is %d, which is exactly equal to your settings.\n", press_rate );
+		fprintf( stdout, "Please refer to the test results as follows: \n" );
+		fprintf( stdout, "\n");
+	}
+	
+	return;
 }
 
 void statistics_calculation( final_statistics *overall_stat, const request_data test_data[], const int press_number )
@@ -146,7 +177,7 @@ void print_statistics( final_statistics *overall_stat )
 	fprintf( stdout, "    HTTP Response Fail Count: %-9d%-8s\n", overall_stat->response_fail_count, "[#]" );
 	fprintf( stdout, "    Overall Fail Count:       %-9d%-8s\n", overall_stat->total_fail_count, "[#]" );
 	fprintf( stdout, "    Total Request Number:     %-9d%-8s\n", overall_stat->total_request, "[#]" );
-	fprintf( stdout, "    Fail Percent:             %.3f%%\n", 100*(overall_stat->fail_percent) );
+	fprintf( stdout, "    Fail Percent:             %.3lf%%\n", 100*(overall_stat->fail_percent) );
 	fprintf( stdout, "    Total Bytes Transferred:  %-9lu%-8s\n", overall_stat->total_byte, "[Bytes]" );
 	fprintf( stdout, "\n" );
 	fprintf( stdout, "Request Time [unit: us]\n" );
